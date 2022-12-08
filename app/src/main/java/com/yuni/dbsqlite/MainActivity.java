279df5e5f5fd7d59d6f973e7c1e37a49c0d776a1
yuniwulandari <115677357+yuniwulandari@users.noreply.github.com>
2022-12-08 12:23:23 +0700

    Initial commit

---
+++ app/src/main/java/com/yuni/dbsqlite/MainActivity.java
@@ -0,0 +1,130 @@
+package com.yuni.dbsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+    public class MainActivity extends AppCompatActivity {
+        DatabaseHelper myDb;
+
+        EditText editNama, editKelas, editNohp, editTextnim,editemail;
+        Button btnAddData;
+        Button btnViewAll;
+        Button btnUpdate;
+        Button btnDelete;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            myDb = new DatabaseHelper(this);
+            editNama = (EditText)findViewById(R.id.editText_nama);
+            editKelas = (EditText)findViewById(R.id.editText_kelas);
+            editNohp = (EditText)findViewById(R.id.editText_nohp);
+            editTextnim = (EditText)findViewById(R.id.editTextnim);
+            editemail=    (EditText)findViewById(R.id.editText_email);
+            btnAddData = (Button)findViewById(R.id.button_add);
+            btnViewAll = (Button)findViewById(R.id.button_view);
+            btnUpdate = (Button)findViewById(R.id.button_update);
+            btnDelete = (Button)findViewById(R.id.button_delete);
+            AddData();
+            viewAll();
+            UpdateData();
+            deleteData();
+        }
+        //fungsi hapus
+        public void deleteData() {
+            btnDelete.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String editText;
+                            Integer deletedRows = myDb.deleteData(editTextnim.getText().toString());
+                            if (deletedRows > 0)
+                                Toast.makeText(MainActivity.this,"Data De leted",Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(MainActivity.this,"Data Failed to  Deleted!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+            );
+        }
+        //fungsi update
+        public void UpdateData() {
+            btnUpdate.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            boolean isUpdate = myDb.updateData
+                                    (editTextnim.getText().toString(),
+                                    editNama.getText().toString(), editKelas.getText().toString(),
+                                    editNohp.getText().toString(), editemail.getText().toString());
+                            if(isUpdate == true)
+                                Toast.makeText(MainActivity.this,"Data Up dated",Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(MainActivity.this,"Data Failed to  Update",Toast.LENGTH_LONG).show();
+                        }
+                    }
+            );
+        }
+        //fungsi tambah
+        public void AddData() {
+            btnAddData.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            boolean isInserted = myDb.insertData(editTextnim.getText().toString(),
+                                    editNama.getText().toString(), editKelas.getText().toString(),
+                                    editNohp.getText().toString(), editemail.getText().toString() );
+                            if(isInserted == true)
+                                Toast.makeText(MainActivity.this,"Data  Iserted",Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(MainActivity.this,"Data Not  Iserted",Toast.LENGTH_LONG).show();
+                            editNama.setText("");
+                            editKelas.setText("");
+                            editTextnim.setText("");
+                            editNohp.setText("");
+                            editemail.setText("");
+                        }
+                    }
+            );
+        }
+        //fungsi menampilkan data
+        public void viewAll() {
+            btnViewAll.setOnClickListener(
+                    new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            Cursor res = myDb.getAllData();
+                            if(res.getCount() == 0) {
+                                // show message
+                                showMessage("Error","Noting Found");
+                                return;
+                            }
+                            StringBuffer buffer = new StringBuffer();
+                            while (res.moveToNext() ) {
+                                buffer.append("NIM :"+ res.getString(0)+"\n");
+                                buffer.append("NAMA :"+ res.getString(1)+"\n");
+                                buffer.append("KELAS :"+ res.getString(2)+"\n");
+                                buffer.append("NOHP :"+ res.getString(3)+"\n\n");
+                                buffer.append("email:"+res.getString(4)+"\n\n");
+
+                            }
+// show all data
+                            showMessage("Data Mahasiswa",buffer.toString());  }
+                    }
+            );
+        }
+        //membuat alert dialog
+        public void showMessage(String title, String Message){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);  builder.setCancelable(true);
+            builder.setTitle(title);
+            builder.setMessage(Message);
+            builder.show();
+        }
+    }
+
+
